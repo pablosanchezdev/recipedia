7683d18ac36dee295887bf051130e9606c4a6ebc
Pablo Sanchez Egido <pablosze@gmail.com>
2017-12-24 14:13:40 +0100

    Added basic routes and controller for recipe resource
    * Basic CRUD routes have been defined
    * Created RecipeController class and methods to handle the routes

---
+++ app/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class RecipeController extends Controller {
+
+    public Result createRecipe() {
+        return ok();
+    }
+
+    public Result retrieveRecipe(Long id) {
+        return ok();
+    }
+
+    public Result updateRecipe(Long id) {
+        return ok();
+    }
+
+    public Result deleteRecipe(Long id) {
+        return ok();
+    }
+
+    public Result retrieveRecipeCollection(Integer page) {
+        return ok();
+    }
+}
